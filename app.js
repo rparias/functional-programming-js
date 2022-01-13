549ba1e91d990f8aad287b6b56fb6507dd61b8e5
Ronald Arias <ariasron@hotmail.com>
2022-01-12 21:40:03 -0500

    Creates pure functions and validate how js shares data by reference

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
-console.log('Hello World')
+const users = [
+  { name: 'James', score: 30, tries: 1 },
+  { name: 'Mary', score: 110, tries: 4 },
+  { name: 'Henry', score: 80, tries: 3 },
+]
+
+// Modifies data
+const storeUser = (users, user) => {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name.toLowerCase() === user.name.toLowerCase()) {
+      users[i] = user
+      break
+    }
+  }
+}
+
+// Pure functions
+const getUser = (users, name) => {
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].name.toLowerCase() === name.toLowerCase()) {
+      return users[i]
+    }
+  }
+  return null
+}
+
+const updateScore = (user, newAmmount) => {
+  if (user) {
+    user.score += newAmmount
+    return user
+  }
+}
+
+const updateTries = (user) => {
+  if (user) {
+    user.tries++
+    return user
+  }
+}
+
+// Checking functions
+const user = getUser(users, 'Henry')
+const user1 = updateScore(user, 30)
+const user2 = updateTries(user1)
+storeUser(users, user2)
+
+console.log(users)
+console.log(user) // This will have the user updated even if we used the pure functions because data is passed by reference
